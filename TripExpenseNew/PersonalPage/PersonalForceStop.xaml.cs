324b5c288a5c1f63e225747bff88214c466402ea
sarit-t <sarit_t@contrologic.co.th>
2025-08-19 15:42:04 +0700

    update Personal ForceStop Page

--- TripExpenseNew/PersonalPage/PersonalForceStop.xaml.cs
+++ TripExpenseNew/PersonalPage/PersonalForceStop.xaml.cs
@@ -1,14 +1,16 @@
-namespace TripExpenseNew.PersonalPage;
+﻿namespace TripExpenseNew.PersonalPage;
 
 public partial class PersonalForceStop : ContentPage
 {
 	public PersonalForceStop()
 	{
 		InitializeComponent();
-	}
 
-    private void PersonalCancel_Clicked(object sender, EventArgs e)
+        timePicker.Time = new TimeSpan(17, 30, 0); // ตั้งเวลาเริ่มต้นเป็น 17:30
+    }
+
+    private async void PersonalForceStopCancel_Clicked(object sender, EventArgs e)
     {
-
+        await Shell.Current.GoToAsync("Home_Page");
     }
 }
