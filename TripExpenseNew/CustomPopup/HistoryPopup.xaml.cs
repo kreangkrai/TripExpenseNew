f0c6d202498d1a82884a1895e094de7c6ec6b6f3
sarit-t <sarit_t@contrologic.co.th>
2025-08-21 17:22:21 +0700

    update: HistoryPopup

---
+++ TripExpenseNew/CustomPopup/HistoryPopup.xaml.cs
@@ -0,0 +1,17 @@
+using CommunityToolkit.Maui.Views;
+using TripExpenseNew.Models;
+
+namespace TripExpenseNew.CustomPopup;
+
+public partial class HistoryPopup : Popup
+{
+	public HistoryPopup()
+	{
+        InitializeComponent();
+	}
+}
+
+//private void CancelBtn_Clicked(object sender, EventArgs e)
+//{
+//	Close(null);
+//}
