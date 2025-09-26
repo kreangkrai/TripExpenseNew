d5bc75027faf24901b4ecbb45ae96fc08ed79acb
kriangkrai rattanawan <kriangkrai@contrologic.co.th>
2025-09-26 13:48:46 +0700

    sss

--- TripExpenseNew/Platforms/Android/MainActivity.cs
+++ TripExpenseNew/Platforms/Android/MainActivity.cs
@@ -5,7 +5,7 @@ using static Android.Content.Res.Resources;
 
 namespace TripExpenseNew
 {
-    [Activity(Theme ="@style/Maui.SplashTheme" ,MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme ="@style/Maui.SplashTheme" ,MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
